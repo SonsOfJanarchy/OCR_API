b5cafdff24eaddcabbb9f095d7e4154342217be5
SonsOfJanarchy <sonsofjanarchy@gmail.com>
2017-12-15 16:25:12 -0600

    Refined OCRRequest definition.

--- OCR_API/OCR_API.ServiceInterface/OCRService.cs
+++ OCR_API/OCR_API.ServiceInterface/OCRService.cs
@@ -14,18 +14,18 @@ namespace OCR_API.ServiceInterface
         {
             OCRResponse myResponse = new OCRResponse();
 
-            if (string.IsNullOrWhiteSpace(request.Base64UploadFile))
+            if (request.Base64UploadFile == null || string.IsNullOrWhiteSpace(request.Base64UploadFile.ToString()))
             {
                 myResponse.jsonResponse = "There was no file attached or no file name was specified.";
                 return myResponse;
             }
 
-            string base64ImgStr = request.Base64UploadFile.Split(',')[1];
+            string base64ImgStr = request.Base64UploadFile.ToString().Split(',')[1];
             byte[] tmpBytes = Convert.FromBase64String(base64ImgStr);
             string fileName = DateTime.Now.Ticks.ToString();
             File.WriteAllBytes(@"C:\temp\OCR\" + fileName, tmpBytes);
 
-            
+            // now do the OCR junk here
 
             myResponse.jsonResponse = "This is the return text.  You've reached the end of the internet...";
             return myResponse;
